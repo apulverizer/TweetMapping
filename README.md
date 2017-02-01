248aaa5020097d11e7dabf1f12ea44e1f6916f84
Aaron Pulver <apulverizer@gmail.com>
2017-01-31 20:32:40 -0500

    Updates with comments and cleaned up code
    Add requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TweetMapping
-A flask app that pushes the most current 200 tweets to a leaflet map.
+A A Flask app that pushes the most current 100 tweets with coordinates to a leaflet map.
+
+# Installation
+ 1. Install Python 2 (if not installed)
+ 2. Clone this repository (or download as zip)
+ 3. Open terminal/cmd and navigate to this repo
+ 4. Run ```pip install -r requirement.txt``` to install all dependencies
+ 5. In main.py input your Twitter Developer/App Access Tokens/Keys. See [here](https://dev.twitter.com/) to sign up for a dev account.
+
+# Running it
+ 1. Run main.py (```python main.py```)
+ 2. In your web browser, navigate to localhost:5000
+ 3. You should see a webmap with blue markers appearing and disappearing
--- main.py
+++ main.py
@@ -1,156 +1,82 @@
+# -*- coding: UTF-8 -*-
 from __future__ import absolute_import, print_function
-
-import json
-from HTMLParser import HTMLParser
-
-from geopy.geocoders import Nominatim
-
-from gevent import monkey
+from gevent import monkey # background threads
 monkey.patch_all()
-
+# import tweepy twitter stuff
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-
-import time
 from threading import Thread
-from flask import Flask, render_template, session, request
-from flask.ext.socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, disconnect
+# import flask stuff
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-consumer_key="<INSERT KEY>"
-consumer_secret="<INSERT KEY>"
-access_token="<INSERT KEY>"
-access_token_secret="<INSERT KEY>"
+# From Twitter Developer Account
+consumer_key = "<Consumer key>"
+consumer_secret = "<Consumer secret>"
+access_token = "<Access Token>"
+access_token_secret = "<Access Token Secret>"
 
+# Flask Stuff
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 thread = None
-geolocator = Nominatim()
 
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    """count = 0
-    while True:
-        time.sleep(10)
-        count += 1
-        socketio.emit('my response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')"""
 
-def geocode_thread(status):
+def tweet_handler(status):
+    """
+    This sends part of the tweet via websocket to the website
+    This could be modified to store tweets in a db or file
+    :param status: (string) The tweet to process
+    :return:
+    """
     status.text.encode('utf8')
+    # if the status has coordinates
     if status.coordinates:
-	socketio.emit('coordinates',
-	   {'lat': status.coordinates['coordinates'][1], 'long': status.coordinates['coordinates'][0], 'text': status.text},
-		      namespace='/test')
-    elif status.user.location:
-	location = None
-	try:
-	    location = geolocator.geocode(status.user.location.encode('utf8'),timeout=5,exactly_one=True)
-	except:
-	    print ("Geocoding Error: " + unicode(status.user.location))
-	    return
-	if location is not None:
-	    socketio.emit('coordinates',{'lat': location.latitude, 'long': location.longitude, 'text': status.text},namespace='/test')
+        # Send some json to the website
+        socketio.emit('coordinates',
+                      {'lat': status.coordinates['coordinates'][1], 'long': status.coordinates['coordinates'][0],
+                       'text': status.text},
+                      namespace='/test')
+        print(status.text)
 
 
 @app.route('/')
 def index():
-    global thread
-    if thread is None:
-        thread = Thread(target=background_thread)
-        thread.start()
+    """
+    Flask route for root of website
+    :return:
+    """
+    # render the index.html template
     return render_template('index.html')
 
 
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('my broadcast event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
-          'count': session['receive_count']})
-
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
-          'count': session['receive_count']})
-
-
-@socketio.on('close room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my room event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-
-@socketio.on('disconnect request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
-
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my response', {'data': 'Connected', 'count': 0})
-
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-
 class MapListener(StreamListener):
-    def on_status(self,status):
-        thread = Thread(target=geocode_thread, args=(status,))
+    """
+    Background thread to process tweets
+    """
+
+    def on_status(self, status):
+        # When we get a new status, process it in the background
+        thread = Thread(target=tweet_handler, args=(status,))
         thread.start()
 
     def on_error(self, status):
         print(status)
 
+
 if __name__ == '__main__':
-    l = MapListener()
+    l = MapListener()  # The background listener
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
     try:
-        stream.filter(locations=[-180,-90,180,90],async=True)
-        #stream.filter(track=["ISIS"],async=True)
+        # Only get tweets with location
+        stream.filter(locations=[-180, -90, 180, 90], async=True)
     except:
-        print ("Exception")
+        # if the stream isns't going or something
+        print("Exception")
+    # run the webserver
     socketio.run(app)
-
-
